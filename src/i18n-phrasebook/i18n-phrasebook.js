f2c3f3a68a72745131109ead4532d17271b968fc
Lawrence Siden <lsiden@gmail.com>
2017-08-23 16:28:27 -0400

    refactored

--- src/i18n-phrasebook/i18n-phrasebook.js
+++ src/i18n-phrasebook/i18n-phrasebook.js
@@ -1,15 +1,20 @@
 const debug = require('debug')('i18n-phrasebook')
 
 export default class {
-	constructor(lang='en', locale={}) {
-		const languages = Object.getOwnPropertyNames(locale)
-		this.lang = languages.indexOf(lang) === -1 ? 'en' : lang
-		this.phrasebook = locale[this.lang] ? locale[this.lang] : {}
+	constructor(phrasebooks={}, lang='en') {
+		this.phrasebooks = phrasebooks
+		this.lang = phrasebooks[lang] ? lang : 'en'
+		this.phrasebook = phrasebooks[this.lang] ? phrasebooks[this.lang] : {}
+		debug('Available languages are: %s', Object.getOwnPropertyNames(phrasebooks).join(', '))
 		debug('language set to %s', this.lang)
-		debug('Phrase book has %d entries', Object.getOwnPropertyNames(this.phrasebook).length)
+		debug('Phrase-book has %d entries for  %s', Object.getOwnPropertyNames(this.phrasebook).length, this.lang)
 	}
 
-	language() {
+	language(lang=null) {
+		if (lang && this.phrasebooks[lang]) {
+			this.lang = lang
+			this.phrasebook = this.phrasebooks[lang]
+		}
 		return this.lang
 	}
 
--- src/i18n-phrasebook/i18n-phrasebook.spec.js
+++ src/i18n-phrasebook/i18n-phrasebook.spec.js
@@ -6,13 +6,19 @@ describe('i18n-phrasebook', () => {
 		expect(phrasebook.language()).toBe('en')
 	})
 
-	it('Set language', () => {
-		const phrasebook = new Phrasebook('en')
+	it('Initialize with language', () => {
+		const phrasebook = new Phrasebook({}, 'en')
 		expect(phrasebook.language()).toBe('en')
 	})
 
 	it('Defaults to en if language is not supported', () => {
-		const phrasebook = new Phrasebook('xxx')
+		const phrasebook = new Phrasebook({}, 'xxx')
+		expect(phrasebook.language()).toBe('en')
+	})
+
+	it('Attempt to change to unsupported language results in no change', () => {
+		const phrasebook = new Phrasebook({}, 'en')
+		phrasebook.language('he')
 		expect(phrasebook.language()).toBe('en')
 	})
 
@@ -21,12 +27,8 @@ describe('i18n-phrasebook', () => {
 		expect(phrasebook.xlate('covfefe')).toBe('covfefe')
 	})
 
-	describe('Add some phrases', () => {
+	describe('Add Spanish phrases', () => {
 		const locale = {
-			en: {
-				'Hello': 'Hello',
-				'Goodby': 'Goodby',
-			},
 			sp: {
 				'Hello': 'Hola',
 				'Goodby': 'Adios'
@@ -34,12 +36,20 @@ describe('i18n-phrasebook', () => {
 		}
 
 		it('translates to English', () => {
-			const phrasebook = new Phrasebook('en', locale)
+			const phrasebook = new Phrasebook(locale, 'en')
 			expect(phrasebook.xlate('Hello')).toBe('Hello')
 		})
 
 		it('translates to Spanish', () => {
-			const phrasebook = new Phrasebook('sp', locale)
+			const phrasebook = new Phrasebook(locale, 'sp')
+			expect(phrasebook.xlate('Hello')).toBe('Hola')
+		})
+
+		it('can change to another supported language', () => {
+			const phrasebook = new Phrasebook(locale, 'en')
+			expect(phrasebook.xlate('Hello')).toBe('Hello')
+			phrasebook.language('sp')
+			expect(phrasebook.language()).toBe('sp')
 			expect(phrasebook.xlate('Hello')).toBe('Hola')
 		})
 	})
